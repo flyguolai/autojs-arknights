a02136f585f39e7fe2056c066e476d07f3efdf26
Ludwig.huang <huang.xuxin@oyohotels.cn>
2019-12-04 19:39:55 +0800

    init

---
+++ scripts/Image/index.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../node_modules/autojs-dev/types/auto.d.ts" />
+
+export class Test {
+    constructor() {
+    }
+
+    test(){
+        toast('test')
+    }
+}
+
+
---
+++ scripts/index.auto.ts
@@ -0,0 +1,15 @@
+/// <reference path="../node_modules/autojs-dev/types/auto.d.ts" />
+
+import {Test} from "./Image";
+
+class Index {
+    constructor() {
+    }
+
+    static main() {
+        new Test().test()
+    }
+}
+
+
+Index.main();
